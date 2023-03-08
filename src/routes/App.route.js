5d134f38d9ef29a60b3871e8a53057225666f7bd
Shaurya <badrinath6024@gmail.com>
2023-03-08 11:58:48 +0530

    reslove persist

--- src/routes/App.route.js
+++ src/routes/App.route.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Home from '../pages/home/Home'
+import Dashboard from '../pages/profile/Dashboard'
 import Login from '../pages/login/Login'
 import TodayEntry from '../pages/diary/TodayEntry'
 import PastEntries from '../pages/diary/PastEntries'
@@ -12,6 +13,7 @@ const AppRoutes = () => {
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/login" element={<Login />}></Route>
+      <Route path="/dashboard" element={<Dashboard />}></Route>
       {/* <Route path='/pageflip' element={<PageFlip />}></Route> */}
       <Route path="/entry" element={<TodayEntry />}></Route>
       <Route path="/pastEntries" element={<PastEntries />}></Route>
